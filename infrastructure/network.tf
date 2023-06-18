2e5cecefe76990c39ef86625aed1485f4bf89cd0
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-06-17 21:19:22 -0400

    Syntax error fix

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,5 +1,5 @@
 module "default_network" {
-  source = "srhoton/tf-module-network.git"
+  source = "srhoton/tf-module-network"
   env_name = var.env_name
   base_cidr_block = var.base_cidr_block
   enable_nat_gateway = false
